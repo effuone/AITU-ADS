26e60b8ea39bd9f2447dcc4c23d739365103ad04
effuone <albkfil@gmail.com>
2024-04-07 23:43:06 +0500

    recursion removed from problem three

--- src/Solutions/ProblemThree.java
+++ src/Solutions/ProblemThree.java
@@ -7,36 +7,38 @@ import java.util.function.Supplier;
 
 public class ProblemThree {
 
-    // Recursive function to check if a number is prime
-    private static boolean isPrime(int n, int i) {
-        if (n <= 2) {
-            return n == 2; // 2 is prime, 1 or less are not
-        }
-        if (n % i == 0) {
-            return false; // If n is divisible by any number other than 1 and itself
-        }
-        if (i * i > n) {
-            return true; // No need to check further if i squared is greater than n
-        }
-        return isPrime(n, i + 1); // Check for next divisor
-    }
-
+    // Function to check if a number is prime
     public static boolean checkPrime(int n) {
-        return isPrime(n, 2); // Start checking from 2
+        if (n <= 1) {
+            return false; // 1 and negative numbers are not prime
+        }
+        if (n == 2) {
+            return true; // 2 is the only even prime number
+        }
+        if (n % 2 == 0) {
+            return false; // Other even numbers are not prime
+        }
+        for (int i = 3; i * i <= n; i += 2) { // Only check the odd numbers
+            if (n % i == 0) {
+                return false; // Found a divisor, n is not prime
+            }
+        }
+        return true; // No divisors found, n is prime
     }
 
     public void demo() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Problem 3: Checking if a number is prime using recursion.");
+        System.out.println("Problem 3: Checking if a number is prime without using recursion.");
 
         System.out.println("Enter a number:");
         int n = scanner.nextInt();
 
+        // Using Supplier to measure execution time
         Supplier<Boolean> checkPrimeSupplier = () -> checkPrime(n);
 
         long timeTaken = Benchmarker.measureExecutionTime(checkPrimeSupplier);
 
-        boolean isPrime = checkPrimeSupplier.get();
+        boolean isPrime = checkPrimeSupplier.get(); // Determine whether is prime using the supplier
 
         System.out.println(n + " is " + (isPrime ? "Prime" : "Composite"));
         System.out.println("Execution time: " + timeTaken + " milliseconds");
