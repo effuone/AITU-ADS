f14b259bd929404b4318a2b47fba4cdd43422cb1
effuone <albkfil@gmail.com>
2024-04-08 02:20:33 +0500

    entry point readability refactor

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,35 @@
 import Solutions.*;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Main {
+    private static final Map<Integer, Consumer<Scanner>> problemActions = new HashMap<>();
+
+    static {
+        problemActions.put(1, scanner -> new ProblemOne().demo());
+        problemActions.put(2, scanner -> new ProblemTwo().demo());
+        problemActions.put(3, scanner -> new ProblemThree().demo());
+        problemActions.put(4, scanner -> new ProblemFour().demo());
+        problemActions.put(5, scanner -> new ProblemFive().demo());
+        problemActions.put(6, scanner -> new ProblemSix().demo());
+        problemActions.put(7, scanner -> new ProblemSeven().demo());
+        problemActions.put(8, scanner -> new ProblemEight().demo());
+        problemActions.put(9, scanner -> new ProblemNine().demo());
+        problemActions.put(10, scanner -> new ProblemTen().demo());
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the problem number to run (e.g., 1 for ProblemOne):");
         int problemNumber = scanner.nextInt();
-        switch (problemNumber) {
-            case 1:
-                ProblemOne problem1 = new ProblemOne();
-                problem1.demo();
-                break;
-            case 2:
-                ProblemTwo problem2 = new ProblemTwo();
-                problem2.demo();
-                break;
-            case 3:
-                ProblemThree problem3 = new ProblemThree();
-                problem3.demo();
-                break;
-            case 4:
-                ProblemFour problem4 = new ProblemFour();
-                problem4.demo();
-                break;
-            case 5:
-                ProblemFive problem5 = new ProblemFive();
-                problem5.demo();
-                break;
-            case 6:
-                ProblemSix problem6 = new ProblemSix();
-                problem6.demo();
-                break;
-            case 7:
-                ProblemSeven problem7 = new ProblemSeven();
-                problem7.demo();
-                break;
-            case 8:
-                ProblemEight problem8 = new ProblemEight();
-                problem8.demo();
-                break;
-            case 9:
-                ProblemNine problem9 = new ProblemNine();
-                problem9.demo();
-                break;
-            case 10:
-                ProblemTen problem10 = new ProblemTen();
-                problem10.demo();
-                break;
-            default:
-                System.out.println("Invalid problem number");
-                break;
+
+        Consumer<Scanner> action = problemActions.get(problemNumber);
+        if (action != null) {
+            action.accept(scanner);
+        } else {
+            System.out.println("Invalid problem number");
         }
     }
 }
